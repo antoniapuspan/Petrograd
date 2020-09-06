7b034d0317b5757a1a6b206d70bde0ec2ca36c19
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2020-09-06 18:35:10 +0200

    vegetarian, if conditons

--- script.js
+++ script.js
@@ -18,12 +18,27 @@ function dataReceived(products) {
 }
 //executed once for each product
 function showProduct(myProduct) {
-    console.log(myProduct)
+    console.log(myProduct) 
 
     //finding the template
     const temp = document.querySelector("#productTemplate").content;
     //clone the template
     const myCopy = temp.cloneNode(true);
+    
+    if(!myProduct.discount) {
+        //console.log("NOT DISCOUNT")
+        myCopy.querySelector(".data_discount").classList.add("hidden")
+        
+    }
+    if(myProduct.vegetarian) {
+        myCopy.querySelector(".vegetarian").classList.remove("hidden");
+    }
+    if(myProduct.soldout) {
+        const p = document.createElement("p");
+        p.textContent = "Sold Out";
+        p.classList.add("soldout")
+        myCopy.querySelector("article").appendChild(p)
+    }
     //fill in the template
 myCopy.querySelector(".data_name").textContent = myProduct.name; 
     //append
